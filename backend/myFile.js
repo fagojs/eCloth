9e067eb1e001507537e9e3fc2e82aaee2eecf4b3
fagojs <sushil.phago123@gmail.com>
2021-07-22 13:25:19 +0545

    added routes files

--- backend/myFile.js
+++ backend/myFile.js
@@ -2,14 +2,16 @@ const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
 
+const userRegisterRoutes = require("./routes/users-register.route");
+const userLoginRoutes = require("./routes/user-login.route");
+
 const app = express();
 
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
-app.get("/", (req, res) => {
-  res.send("hello from  here");
-});
+app.use("/register", userRegisterRoutes);
+app.use("/login", userLoginRoutes);
 
 app.listen(port, () => {
   console.log(`Listening to port:${port}`);
