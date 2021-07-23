54614d16293fc56bf8e758b4212329d0b50ac2f5
fagojs <sushil.phago123@gmail.com>
2021-07-23 16:50:19 +0545

    used cors middleware

--- backend/myFile.js
+++ backend/myFile.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 require("dotenv").config();
 
 const userRegisterRoutes = require("./routes/users-register.route");
@@ -9,6 +10,7 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(express.json());
 app.use("/register", userRegisterRoutes);
 app.use("/login", userLoginRoutes);
