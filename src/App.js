ee1bb7e4229dc2139dba386d17b6ecd69baebdb1
fagojs <sushil.phago123@gmail.com>
2021-07-23 12:01:57 +0545

    path defined for register-form

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Route, Switch } from "react-router-dom";
 import HomePage from "./components/home/homePage.component";
 import ShopPage from "./components/shop/shop.component";
 import Header from "./components/header/header.component";
-import SignIn from "./components/signin-signup/signin.component";
+import SignInUp from "./components/signin-signup/signInUp.component";
 
 import "./css/App.css";
 
@@ -13,7 +13,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
-        <Route path="/signin" component={SignIn} />
+        <Route path="/register-form" component={SignInUp} />
       </Switch>
     </div>
   );
