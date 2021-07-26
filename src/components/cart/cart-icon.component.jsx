a0d4d5d99cd9b36ad8b59178d24cfb9a06e1c888
fagojs <sushil.phago123@gmail.com>
2021-07-26 13:57:51 +0545

    counts total quantity inside cart

--- src/components/cart/cart-icon.component.jsx
+++ src/components/cart/cart-icon.component.jsx
@@ -4,15 +4,25 @@ import { toggleDropdown } from "../../redux/actions/cart.action";
 import { AiOutlineShopping } from "react-icons/ai";
 import "../../css/cart/cart-icon.css";
 
-const CartIcon = ({ toggleDropdown }) => (
-  <div className="cart-icon" onClick={toggleDropdown}>
-    <AiOutlineShopping className="shop-box" />
-    <span className="item-count">0</span>
-  </div>
-);
+const CartIcon = ({ toggleDropdown, cartItems }) => {
+  let totalQuantity = 0;
+  cartItems.map((cartItem) => {
+    return (totalQuantity += cartItem.quantity);
+  });
+  return (
+    <div className="cart-icon" onClick={toggleDropdown}>
+      <AiOutlineShopping className="shop-box" />
+      <span className="item-count">{totalQuantity}</span>
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => ({
+  cartItems: state.cart.cartItems,
+});
 
 const mapDispatchToProps = (dispatch) => ({
   toggleDropdown: () => dispatch(toggleDropdown()),
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
