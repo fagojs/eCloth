5a89da03aecb14d51c3a29479605a8ca3e06848d
fagojs <sushil.phago123@gmail.com>
2021-07-29 17:50:15 +0545

    collection extracted from store and implemented on collection-page with styling

--- src/components/shop/collection-page.component.jsx
+++ src/components/shop/collection-page.component.jsx
@@ -1,10 +1,24 @@
-const CollectionPage = ({ match }) => {
-  console.log(match.params.collectionId);
-  return (
-    <div className="collection-page">
-      <h1>Collection</h1>
-    </div>
-  );
-};
+import { connect } from "react-redux";
 
-export default CollectionPage;
+import CollectionItem from "./collection-item.component";
+import { selectSpecificCollection } from "../../redux/selectors/shop.selector";
+
+import "../../css/shop/collection-page.css";
+
+const CollectionPage = ({ collection }) => (
+  <div className="collection-page">
+    <h1>{collection.title.toUpperCase()}</h1>
+    <div className="items">
+      {collection.items.map((item) => (
+        <CollectionItem key={item.id} item={item} />
+      ))}
+    </div>
+  </div>
+);
+
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectSpecificCollection(ownProps.match.params.collectionId)(
+    state
+  ),
+});
+export default connect(mapStateToProps)(CollectionPage);
